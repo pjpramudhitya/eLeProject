a1e495b2a1eac415434eadefdfdcb5b5f9270877
Ponang Jagad Pramudhitya <32562698+pjpramudhitya@users.noreply.github.com>
2018-11-12 10:00:33 +0700

    Pertama

---
+++ app/src/main/java/com/example/pjpramudhitya/e_le/KolamHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.pjpramudhitya.e_le;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class KolamHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kolam_home);
+    }
+}

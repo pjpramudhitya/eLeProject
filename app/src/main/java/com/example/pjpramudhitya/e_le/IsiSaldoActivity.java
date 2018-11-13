5c1eaf1cd824de288084039386c6468e3e47b02b
Ponang Jagad Pramudhitya <32562698+pjpramudhitya@users.noreply.github.com>
2018-11-13 07:28:06 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/pjpramudhitya/eLeProject""
    This reverts commit f0239d8c262d82bd6a48a935426918bfd41a64a1.

--- app/src/main/java/com/example/pjpramudhitya/e_le/IsiSaldoActivity.java
+++ app/src/main/java/com/example/pjpramudhitya/e_le/IsiSaldoActivity.java
@@ -1,4 +1,17 @@
 package com.example.pjpramudhitya.e_le;
 
+<<<<<<< HEAD
 public class IsiSaldoActivity {
+=======
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class IsiSaldoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isi_saldo);
+    }
+>>>>>>> 053265a521d81e64fdea01b1243d7fd7cfcc816a
 }

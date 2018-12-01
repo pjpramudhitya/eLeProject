0d8add9d0209fa700c2b92fe5bdb676e483bc714
Ponang Jagad Pramudhitya <32562698+pjpramudhitya@users.noreply.github.com>
2018-12-01 14:49:25 +0700

    Firebase

--- app/src/main/java/com/example/pjpramudhitya/e_le/KolamProfileActivity.java
+++ app/src/main/java/com/example/pjpramudhitya/e_le/KolamProfileActivity.java
@@ -2,15 +2,64 @@ package com.example.pjpramudhitya.e_le;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class KolamProfileActivity extends AppCompatActivity{
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference mRef = database.getReference("https://eleproject-2d230.firebaseio.com/Users/gibs");
+    private TextView txt_nama, txt_username, txt_alamat, txt_email, txt_notelp, txt_pin, txt_saldo, txt_password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kolam_profile);
+
+        txt_nama = (TextView) findViewById(R.id.txt_nama);
+        txt_username = (TextView) findViewById(R.id.txt_username);
+        txt_alamat = (TextView) findViewById(R.id.txt_alamat);
+        txt_email = (TextView) findViewById(R.id.txt_email);
+        txt_notelp = (TextView) findViewById(R.id.txt_noHP);
+        txt_pin = (TextView) findViewById(R.id.txt_pin);
+        txt_password = (TextView) findViewById(R.id.txt_password);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, String> map = dataSnapshot.getValue(Map.class);
+
+                String name = map.get("Name");
+                String email = map.get("email");
+                String no_telp = map.get("no_telp");
+                String alamat = map.get("alamat");
+                String pin_lepay = map.get("pin_lepay");
+                String saldo_lepay = map.get("saldo_lepay");
+
+                txt_nama.setText(name);
+                txt_email.setText(email);
+                txt_notelp.setText(no_telp);
+                txt_alamat.setText(alamat);
+                txt_pin.setText(pin_lepay);
+                txt_saldo.setText(saldo_lepay);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void saveBtn(View v){
--- app/src/main/java/com/example/pjpramudhitya/e_le/LoginActivity.java
+++ app/src/main/java/com/example/pjpramudhitya/e_le/LoginActivity.java
@@ -3,14 +3,31 @@ package com.example.pjpramudhitya.e_le;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mUsernameView;
     private EditText mPasswordView;
+    private Button btnSignIn;
+
+    //firebase
+    FirebaseDatabase database;
+    DatabaseReference users;
+    DatabaseReference seller;
 
 
     @Override
@@ -20,11 +37,21 @@ public class LoginActivity extends AppCompatActivity {
 
         mUsernameView = (EditText) findViewById(R.id.txt_username);
         mPasswordView = (EditText) findViewById(R.id.txt_password);
+        btnSignIn = (Button) findViewById(R.id.btn_login);
+
+        //firebase
+        //database = FirebaseDatabase.getInstance().getReference("https://eleproject-2d230.firebaseio.com/");
+
 
     }
 
     public void login(View v){
-        String username = (String) mUsernameView.getText().toString();
+        final String username = (String) mUsernameView.getText().toString();
+        final String passwordTxt = (String) mPasswordView.getText().toString();
+
+
+
+
 
         if (username.equalsIgnoreCase("seller")){
             startActivity(new Intent(LoginActivity.this, SellerHomeActivity.class));
